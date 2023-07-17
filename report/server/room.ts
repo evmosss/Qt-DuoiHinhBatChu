b3c0c9d6be0639bdc8326c7855f0cd23e3548571
Evmos <rocketme144@gmail.com>
2023-07-17 21:01:01 +0700

    update report

--- report/server/room.ts
+++ report/server/room.ts
@@ -1,40 +1,48 @@
 export enum RoomStatus {
-    PENDING,
-    FULL,
-    STARTED,
-    FINISHED,
+  PENDING,
+  FULL,
+  STARTED,
+  FINISHED,
+}
+
+export enum RoomDifficulty {
+  EASY,
+  MEDIUM,
+  HARD,
+  ADVANCED,
 }
 
 /**
- * @param players: list of players 
+ * @param players: list of players
  *                 based on their id
- * @param points: list of points relative 
+ * @param points: list of points relative
  *                to players by index
  * @param questionIndex: the current question
  * @param status: room status
  */
 export interface RoomData {
-    roomId: string,
-    players: [number],
-    points: [number],
-    ownerId: number,
-    status: RoomStatus,
-    questionIndex: number,
-    maxQuestions: number,
+  roomId: string;
+  players: [number];
+  points: [number];
+  ownerId: number;
+  status: RoomStatus;
+  questionIndex: number;
+  maxQuestions: number;
+  difficulty: RoomDifficulty;
 }
 
 /**
- * @dev Use to send answer (whether the answer 
- *      is true) to all client in a room when 
+ * @dev Use to send answer (whether the answer
+ *      is true) to all client in a room when
  *      a participant send a answer
  * @param roomDetail: for updating point
  * @param isTrue: check whether the answer
  *                from client is true
- * @param content: message will be appended 
+ * @param content: message will be appended
  *                 to chat board of that room
  */
 export interface SendAnswer {
-    roomDetail: RoomData,
-    isTrue: boolean,
-    content: string,
+  roomDetail: RoomData;
+  isTrue: boolean;
+  content: string;
 }
